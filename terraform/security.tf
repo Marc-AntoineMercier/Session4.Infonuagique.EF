9b5de836a287cdf065d3cc8b043be3267cb79f76
root <root@cloud-2-merciermar.10.10.0.5>
2025-05-12 19:39:31 +0000

    tenter de faire fonctionner

--- terraform/security.tf
+++ terraform/security.tf
@@ -6,14 +6,14 @@ resource "aws_security_group" "https_access" {
   ingress {
 
     description = "HTTPS"
-    from_port   = 0
-    to_port     = 0
+    from_port   = "-1"
+    to_port     = "-1"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port        = 0
-    to_port          = 0
+    from_port        = "0"
+    to_port          = "0"
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
